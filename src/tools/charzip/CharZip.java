f45842022c97524ddabe81540ef301d494631b6e
DQMT <daoqiemantou@gmail.com>
2017-11-03 09:27:17 +0800

    charZip

--- src/tools/charzip/CharZip.java
+++ src/tools/charzip/CharZip.java
@@ -1,53 +1,38 @@
 package tools.charzip;
 
+import tools.coolbyte.CoolBytes;
+
 import java.io.*;
 
+/**
+ * charZip :  file            ---> hex string text
+ * deCharZip: hex string text ---> file
+ */
 public class CharZip {
     public static void main(String[] args) {
         String source = "C:\\Users\\Administrator\\git\\springBootDemo.7z";
         String target = "C:\\Users\\Administrator\\git\\charzip.txt";
         try {
-//            charZip(source,target);
-            saveB(source, target);
-            //readB(target, source);
-            //deCharZip(target);
+            //charZip(source, target);
+            deCharZip(target, source);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void charZip(String source, String target) throws IOException {
-        File sourceFile = new File(source);
-        File targetFile = new File(target);
-        FileOutputStream out = new FileOutputStream(targetFile);
-        ObjectOutputStream oos = new ObjectOutputStream(out);
-        oos.writeObject(sourceFile);
-        oos.flush();
-        oos.close();
-        out.close();
-    }
-
-    public static void deCharZip(String source) throws IOException, ClassNotFoundException {
-        File sourceFile = new File(source);
-        FileInputStream fis = new FileInputStream(sourceFile);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        File file = (File) ois.readObject();
-        file.createNewFile();
-        ois.close();
-        fis.close();
-    }
-
-
-    public static void saveB(String source, String target) {
+    public static void charZip(String source, String target) {
         DataInputStream dataInputStream = null;
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(target);
             dataInputStream = new DataInputStream(new FileInputStream(source));
             int temp;
             while ((temp = dataInputStream.read()) != -1) {
-                String  s = Integer.toBinaryString(temp);
-                fileOutputStream.write(s.getBytes());
-                System.out.println(s);
+                String hex = Integer.toHexString(temp).toUpperCase();
+                if(hex.length()==1){
+                    hex = "0"+hex;
+                }
+                fileOutputStream.write(hex.getBytes());
+                System.out.println(hex);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -62,36 +47,20 @@ public class CharZip {
         }
     }
 
-    public static void readB(String source, String target) {
+    public static void deCharZip(String source, String target) {
         try {
             DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(target));
-            FileInputStream fileInputStream = new FileInputStream(source);
+            FileReader fileReader = new FileReader(source);
             int temp;
             StringBuilder sb = new StringBuilder();
-            while ((temp = fileInputStream.read()) != -1) {
-
-                sb.append(temp);
-                dataOutputStream.write(temp);
+            while ((temp = fileReader.read()) != -1) {
+                sb.append((char)temp);
+                System.out.println((char)temp);
             }
+            byte[] bytes = CoolBytes.hexStringToBytes(sb.toString());
+            dataOutputStream.write(bytes);
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    public static ByteArrayOutputStream readInputStream(InputStream is) {
-        try {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            int length = 0;
-            byte[] buffer = new byte[1024];
-            while ((length = is.read(buffer)) != -1) {
-                baos.write(buffer, 0, length);
-            }
-            is.close();
-            baos.close();
-            return baos;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
         }
     }
 
